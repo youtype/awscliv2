6280d7f91fdd93b5e58267280a9321e00c307dd2
Vlad Emelianov <volshebnyi@gmail.com>
2022-05-26 01:37:38 +0300

    Update example

--- scripts/example.py
+++ scripts/example.py
@@ -1,3 +1,4 @@
+import json
 import shlex
 
 from awscliv2.api import AWSAPI
@@ -6,5 +7,8 @@ aws_api = AWSAPI()
 output = aws_api.execute(
     shlex.split("ec2 describe-instances  --filters 'Name=instance-type,Values=t2.micro'")
 )
+data = json.loads(output)
+instances = data["Reservations"][0]["Instances"]
+instance_ids = [i["InstanceId"] for i in instances]
 
-print("Output:", output)
+print("Instance IDs:", instance_ids)
